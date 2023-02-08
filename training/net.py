added global average pooling and fixed validation transform

training/net.py
@@ -5,27 +5,29 @@ from torchvision import models
 
 
 class Net(nn.Module):
-    def __init__(self, feat_vect_dim):
+    def __init__(self, feat_vect_dim, use_avg_pool):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(feat_vect_dim, 120)
+        self.avgpool = nn.AdaptiveAvgPool2d((4, 4)) if use_avg_pool else nn.Identity()
+        self.fc1 = nn.Linear(256 if use_avg_pool else feat_vect_dim, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 1)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.avgpool(x)
         x = torch.flatten(x, 1) # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-def load_model(model_name, feat_vect_dim=None):
+def load_model(model_name, feat_vect_dim=None, use_avg_pool=None):
     if model_name == "Net":
-        return Net(feat_vect_dim) 
+        return Net(feat_vect_dim, use_avg_pool) 
 
     elif model_name == "mobilenet_v2":
         model = models.mobilenet_v2(pretrained=True)
@@ -37,7 +39,8 @@ def load_model(model_name, feat_vect_dim=None):
         
         model = nn.Sequential(
             model,
-            nn.Linear(feat_vect_dim, 1)
+            nn.AdaptiveAvgPool2d((4, 4)) if use_avg_pool else nn.Identity(),
+            nn.Linear(256 if use_avg_pool else feat_vect_dim, 1)
         )
 
         return model

training/steerDS.py
@@ -16,11 +16,15 @@ class SteerDataSet(Dataset):
         self.img_ext = img_ext        
         self.filenames = glob(path.join(self.root_folder,"*/*" + self.img_ext))
         self.totensor = transforms.ToTensor()
+        # set this in the training loop, at each epoch for each phase
+        self.phase = None
         
     def __len__(self):        
         return len(self.filenames)
     
     def __getitem__(self,idx):
+        assert self.phase in ["train", "val"]
+
         f = self.filenames[idx]        
         img = cv2.imread(f)
         
@@ -31,14 +35,15 @@ class SteerDataSet(Dataset):
         steering = f.split("/")[-1].split(self.img_ext)[0][6:]
         steering = np.float32(steering)        
 
-        if torch.randint(0, 2, (1,)).item():
-            img = cv2.flip(img, 1)
-            steering *= np.array(-1, dtype=steering.dtype)
+        if self.phase == "train":
+            if torch.randint(0, 2, (1,)).item():
+                img = cv2.flip(img, 1)
+                steering *= np.array(-1, dtype=steering.dtype)
 
         if self.transform == None:
             img = self.totensor(img)
         else:
-            img = self.transform(img)   
+            img = self.transform[self.phase](img)   
         
         sample = {"image":img , "steering":steering}        
         

training/train.py
@@ -18,16 +18,28 @@ from training.steerDS import SteerDataSet
 
 
 def main(args):
-    # Load data
+    transform = {}
     if args.model_name == "Net":
-        transform = transforms.Compose([
+        transform["train"] = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+            )
+        ])
+        transform["val"] = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(
                 (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
             )
         ])
     elif args.model_name == "mobilenet_v2":
-        transform = transforms.Compose([
+        transform["train"] = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            )
+        ])
+        transform["val"] = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(
                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
@@ -36,6 +48,7 @@ def main(args):
     else:
         raise NotImplementedError
 
+    # Load data
     ds = SteerDataSet(
         os.path.join(os.getcwd(), "data"),
         args.crop_ratio,
@@ -54,7 +67,7 @@ def main(args):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     # Create network
-    net = load_model(args.model_name, args.feat_vect_dim)
+    net = load_model(args.model_name, args.feat_vect_dim, args.use_avg_pool)
     net.to(device)
 
     # Define loss and optim
@@ -87,6 +100,7 @@ def main(args):
             is_training = True if p == "train" else False
 
             net.train(is_training)
+            dataloader[p].dataset.dataset.phase = p
             with torch.set_grad_enabled(is_training):
 
                 for i, data in enumerate(dataloader[p], start=1):
@@ -166,6 +180,7 @@ if __name__ == "__main__":
     parser.add_argument("--train_split", type=float, default=0.7)
     parser.add_argument("--num_epochs", type=int, default=5)
     parser.add_argument("--feat_vect_dim", type=int, default=48048)
+    parser.add_argument("--use_avg_pool", action="store_true")
     parser.add_argument("--min_max_boundaries", type=str, default="-0.5,0.5")
     parser.add_argument("--model_name", type=str, default="Net")
     parser.add_argument("--seed", type=int, default=10)