970d162ea0e5f42b0d257d731822ead01b1a17dd
gabrielecaddeo <caddeogabrielemario@gmail.com>
2023-02-08 11:38:39 +0000

    Add fix to count labels

--- training/count_labels.py
+++ training/count_labels.py
@@ -1,23 +1,30 @@
 import torch
 from torch.utils.data import DataLoader, random_split
-from training.steerDS import SteerDataSet
+from steerDS import SteerDataSet
+from torchvision import transforms
+
 
 transform = transforms.Compose([
         transforms.ToTensor(),
 
     ])
 ds = SteerDataSet(
-    os.path.join(os.getcwd(), "data"),
+    "/home/user/images/",
+    0.35,
     ".jpg",
     transform
 )
 
 print("The dataset contains %d images " % len(ds))
 
-trainset, valset = random_split(ds, [args.train_split, args.val_split])
+trainset, valset = random_split(
+        ds, [0.7, 0.3],
+        generator=torch.Generator().manual_seed(10)
+    )
 
-trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True)
-dict_classes = {'0': 0, '-0.1': 0, '-0.2': 0,
+
+trainloader = DataLoader(trainset, batch_size=4, shuffle=True)
+dict_classes = {'0.0': 0, '-0.1': 0, '-0.2': 0,
 '-0.3': 0,
 '-0.4': 0,
 '-0.5': 0,
@@ -25,11 +32,13 @@ dict_classes = {'0': 0, '-0.1': 0, '-0.2': 0,
 '0.2': 0,
 '0.3': 0,
 '0.4': 0,
-'0.5': 0}
+                '0.5': 0,
+                '-0.0': 0}
 
 for data in trainloader:
     label = data['steering']
 
-    dict_classes[str(label)] += 1
+    for i in range(label.shape[0]):
+        dict_classes[str(round(label[i].item(),2))] += 1
 
 print(dict_classes)
